1e901e5153ce26c128ec6a1d6a48e5f53ea1ec2b
QUADEH <127901701+QUADEH@users.noreply.github.com>
2023-03-22 13:30:53 -0500

    Update reverse.js

--- reverse.js
+++ reverse.js
@@ -1,6 +1,16 @@
-let input = document.querySelector('#input');
-let output = document.querySelector('#output');
+let reverse = document.querySelector("#reverse");
+let input = document.querySelector("#input");
+let output = document.querySelector("#output");
 
-input.addEventListener('input', function(e) {
-  output.textContent = e.target.value.split('').reverse().join('');
-});
+function reverseText(text) {
+  let reversedText = text.split("").reverse().join("");
+  return reversedText
+}
+
+function handleClick () {
+  let text = input.textContent;
+  let reversed = reverseText(text);
+  output.textContent = reversed;
+}
+
+reverse.addEventListener('click', handleClick);
