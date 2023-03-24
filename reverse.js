ba7ce6a17df15311612e687b568ac36211d7abea
QUADEH <127901701+QUADEH@users.noreply.github.com>
2023-03-23 21:07:53 -0500

    Update reverse.js

--- reverse.js
+++ reverse.js
@@ -2,6 +2,7 @@ let reverse = document.querySelector("#reverse");
 let rot13 = document.querySelector("#rot13");
 let input = document.querySelector("#input");
 let output = document.querySelector("#output");
+let toBinary = document.querySelector("#toBinary");
 
 function reverseText(text) {
   let reversedText = text.split("").reverse().join("");
@@ -23,6 +24,17 @@ function rot13Text(text) {
   return rot13Text;
 }
 
+function toBinaryText(text) {
+  let binaryText = "";
+  for (let i = 0; i < text.length; i++) {
+    let charCode = text.charCodeAt(i);
+    let binaryChar = charCode.toString(2);
+    binaryText += binaryChar.padStart(8, "0") + " ";
+  }
+  return binaryText.trim();
+}
+
+
 function handleClickReverse() {
   let text = input.value;
   let reversed = reverseText(text);
@@ -34,6 +46,12 @@ function handleClickRot13() {
   let rot13ed = rot13Text(text);
   output.value = rot13ed;
 }
+function handleClickToBinary() {
+  let text = input.value;
+  let binary = toBinaryText(text);
+  output.value = binary;
+}
 
 reverse.addEventListener('click', handleClickReverse);
 rot13.addEventListener('click', handleClickRot13);
+toBinary.addEventListener('click', handleClickToBinary);
